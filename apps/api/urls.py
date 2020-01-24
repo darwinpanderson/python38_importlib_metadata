fbc0c5800bedefa2d1977c6b00936425b5bf5bc9
DarwinPAnderson <darwin.p.anderson@gmail.com>
2020-01-24 00:36:14 -0600

    Added metadata and version methods Added gitignore file

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('list/', views.listpkg, name='list'),
+    path('list/<str:package>', views.listpkg, name='list_package'),
 ]
--- apps/api/views.py
+++ apps/api/views.py
@@ -1,22 +1,61 @@
 from django.http import HttpResponse, JsonResponse
-from pkg_resources import working_set
-import re, json
+import re, json, pkgutil, importlib.metadata
 
 def index(request):
     """
-    API endpoint that presents all installed .
+    API endpoint that presents all endpoints.
     """
-    dists = [d for d in working_set]
-    dist_list = []
-    pattern = "(\w+)(\s)(...+)"
-    for d in dists:
-        mod = {
-            "name":"",
-            "version":"",
+    endpoints = [
+        {
+            "endpoint":"list/",
+            "desc":"Lists valid packages installed via Django",
+            "parameters":"none"
+        },{
+            "endpoint":"list/<package>",
+            "desc":"Lists all details of supplied package",
+            "parameters":"package"
         }
-        result = re.match(pattern, str(d))
-        mod['name'] = result[1]
-        mod['version'] = result[3]
-        dist_list.append(mod)
+    ]
 
+    return JsonResponse(endpoints, safe=False)
+
+def listpkg(request,package=None):
+    """
+    Lists all valid packages installed.
+    """
+    dist_list = getpackages(package)
     return JsonResponse(dist_list, safe=False)
+
+def getpackages(package=None):
+    """
+    Helper function, returns a list of packages. 
+    """
+    dist_list = []
+    if package:
+        pkgi = getpackageinfo(package)
+        dist_list.append(pkgi)
+    else:
+        pattern="ModuleInfo\(module_finder=FileFinder\([0-9a-zA-Z'\/\-\_\.\/]+\), name='([\w]+)', ispkg=True\)"
+        for p in pkgutil.iter_modules():
+            result = re.match(pattern, str(p))
+            if result:
+                try:
+                    pkgi = getpackageinfo(result[1])
+                    dist_list.append(pkgi)
+                except:
+                    pass              
+    return dist_list
+
+def getpackageinfo(package):
+    pkgi = {}
+    pkgi['name'] = package
+    pkgi['version'] = importlib.metadata.version(package)
+    try:
+        md_list = []
+        md = {}
+        for i in importlib.metadata.metadata(package).items():
+            md[i[0]] = i[1]
+        pkgi['metadata'] = md
+    except Exception as e:
+        pkgi['metadata'] = str(e)
+    return pkgi
